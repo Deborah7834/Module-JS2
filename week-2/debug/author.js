175f87db22ba1d9a58956efa104f452d8a32b6a5
¨Deborah <deborahgamedze7@gmail.com>
2024-11-30 12:58:07 +0100

    solved exercise by correcting mistakes

--- week-2/debug/author.js
+++ week-2/debug/author.js
@@ -11,6 +11,10 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
+/*
+
+Object.values(author) returns an array of all the values in the author object: ["Zadie", "Smith", "writer", 40, true].
+The for...of loop iterates over this array, logging each value*/
